6d33eec0d9798da73001a34bcbfd3a43b160bf8c
Frank Cruz <frankcruz0806@gmail.com>
2020-05-20 21:36:30 -0500

    Class 3:  Reading Query String Request Parameters
    *This lesson teaches us how to create queries and receive them in the backend using @PathParam() method
    	-For example: another method was created and in this its params are set as follow:
    		* @PathParam(value = "page") int page, @PathParam(value = "example") int example
    	- Doing this allows us to get those values passed and make use of them

--- src/main/java/com/sprintbootproject/backendproject/ui/controller/UserController.java
+++ src/main/java/com/sprintbootproject/backendproject/ui/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.sprintbootproject.backendproject.ui.controller;
 
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("users") //http://localhost:8080/users
 public class UserController {
  
+ @GetMapping
+ public String getUsers(@PathParam(value = "page") int page, @PathParam(value = "limit") int limit){
+  return "Get users with params was called with page= " + page + " and limit= "+ limit;
+ }
+
  @GetMapping(path="/{userId}")
  public String getUser(@PathVariable String userId){
   return "Get user was called with userId= " + userId;
