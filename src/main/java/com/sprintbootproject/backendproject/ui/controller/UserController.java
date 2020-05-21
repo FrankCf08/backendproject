8b04adc1bdba227e2cc727a74da9cadc84bb0a44
Frank Cruz <frankcruz0806@gmail.com>
2020-05-20 22:44:51 -0500

    Class 4: Making Params Optional or Requiered
    * This lesson we use of the defaultValue from @RequestParam in-build method, which allows us to set/define a default value for our params if they don't have any value
    * It also shows us to use "requiered = boolean", if the value is not set. Although, this value is only used in String as converters the null pointer to a null
    	-Example:
    	 public String getUsers(@RequestParam(value = "page", defaultValue ="25") int page,
    	 @RequestParam(value = "sort", defaultValue = "desc", required = false) String sort){

--- src/main/java/com/sprintbootproject/backendproject/ui/controller/UserController.java
+++ src/main/java/com/sprintbootproject/backendproject/ui/controller/UserController.java
@@ -1,14 +1,13 @@
 package com.sprintbootproject.backendproject.ui.controller;
 
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,8 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
  
  @GetMapping
- public String getUsers(@PathParam(value = "page") int page, @PathParam(value = "limit") int limit){
-  return "Get users with params was called with page= " + page + " and limit= "+ limit;
+ public String getUsers(@RequestParam(value = "page", defaultValue ="25") int page, 
+  @RequestParam(value = "limit", defaultValue = "25") int limit,
+  @RequestParam(value = "sort", required = false) String sort){
+  return "Get users with params was called with page= " + page + ", limit= "+ limit + " and sort= " + sort;
  }
 
  @GetMapping(path="/{userId}")
