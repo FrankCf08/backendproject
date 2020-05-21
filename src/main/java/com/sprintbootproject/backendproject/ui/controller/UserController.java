5e7080c92acf87a9aff92f03c204c6fb0f6f5bd4
Frank Cruz <frankcruz0806@gmail.com>
2020-05-20 20:37:55 -0500

    Class2: Reading Path Variables with @PathVariables annotation
    * This lesson shows us to read variables pass as variables in the URL
    	-Example: users/HBDAHJNdasdna54eq
    		*Which users is my collection and HBDAHJNdasdads54eq my "userId"
    * This is achieved by :
    	-  @GetMapping(path="/{userId}")
    	- And then using this in my method:
    		 public String getUser(@PathVariable String userId)

--- src/main/java/com/sprintbootproject/backendproject/ui/controller/UserController.java
+++ src/main/java/com/sprintbootproject/backendproject/ui/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.sprintbootproject.backendproject.ui.controller;
 
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("users") //http://localhost:8080/users
 public class UserController {
  
- @GetMapping
- public String getUser(){
-  return "Get user was called";
+ @GetMapping(path="/{userId}")
+ public String getUser(@PathVariable String userId){
+  return "Get user was called with userId= " + userId;
  }
 
  @PostMapping
